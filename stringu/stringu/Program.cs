e69400075982355096d41194aa88e28a8f608be7
ibrohim795 <iismatov59@gmail.com>
2023-07-19 21:51:16 +0500

    bajarildi

--- stringu/stringu/Program.cs
+++ stringu/stringu/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace stringu
+﻿using Microsoft.VisualBasic;
+
+namespace stringu
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ibrohim ismato");
+            char hr = 'g';
+            int x = 0, xmax=0;
+            string a=Console.ReadLine();
+            for(int i=0; i<a.Length; i++)
+            {
+                for (int j =i ; j< a.Length-1; j++)
+                {
+                    if (a[i] == a[j])
+                    {
+                        x++;
+                    }
+                    else break;
+
+                }
+                if (x > xmax)
+                {
+                    xmax = x;
+                     hr = a[i];
+                }x=0;
+
+            }
+            Console.WriteLine($"{hr} harfdan {xmax} ta bor");
         }
     }
 }
