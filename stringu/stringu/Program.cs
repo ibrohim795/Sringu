18e370ab2c9d1117610f5487771b6b192be37647
ibrohim795 <iismatov59@gmail.com>
2023-07-20 15:19:39 +0500

    bajarildi

--- stringu/stringu/Program.cs
+++ stringu/stringu/Program.cs
@@ -1,12 +1,34 @@
-﻿namespace stringu
+﻿using System.Threading.Channels;
+
+namespace stringu
 {
     internal class Program
     {
         static void Main(string[] args)
         {
 
-           
+            string s = Console.ReadLine();
+             unliundosh(s);
         }
-        
+
+        private static string unliundosh(string s)
+        {
+            int unli = 0, undosh = 0;
+            s = s.ToLower();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'a' || s[i] == 'o' || s[i] == 'u' || s[i] == 'i' || s[i] == 'e')
+                {
+                    unli++;
+                }
+                else if (char.IsLetter(s[i]) && !(s[i] == 'a' || s[i] == 'o' || s[i] == 'u' || s[i] == 'i' || s[i] == 'e')) 
+                {
+                    undosh++;
+                }
+            }
+            Console.WriteLine($"{undosh} undosh harflar");
+            Console.WriteLine($"{unli} unli harflar");
+            return s;
+        }
     }
 }
