6e411fb033fe06bc2ed18d8bb0bfcad4070ab678
ibrohim795 <iismatov59@gmail.com>
2023-07-20 13:48:57 +0500

    bajarildi

--- stringu/stringu/Program.cs
+++ stringu/stringu/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ibrohim");
+            Console.Write("Ismingizni kiriting: ");
+            string ism = Console.ReadLine();
+            string natija = AylandirilganIsm(ism);
+            Console.WriteLine("Chiqish: " + natija);
+        }
+
+        static string AylandirilganIsm(string ism)
+        {
+            string[] ismList = ism.Split(' ');
+            for (int i = 0; i < ismList.Length; i++)
+            {
+                string currentIsm = ismList[i];
+                if (currentIsm.Length > 0)
+                {
+                    ismList[i] = char.ToUpper(currentIsm[0]) + ".";
+                }
+            }
+
+            return string.Join(" ", ismList);
         }
     }
 }
