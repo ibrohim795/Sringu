6e8e0883588f2ad19943975884dd40c53cdb60ee
ibrohim795 <iismatov59@gmail.com>
2023-07-20 14:31:19 +0500

    bajarildi

--- stringu/stringu/Program.cs
+++ stringu/stringu/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string s=Console.ReadLine();
+            //Console.WriteLine(s);
+            Console.WriteLine(engks(s));
+        }
+
+        static string engks(string s)
+        {
+            string[] str = s.Split(' ');
+            return str.Max() ;
         }
     }
 }
